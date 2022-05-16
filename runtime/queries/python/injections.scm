ab3e65a5df892429fdde76751f2081aaf10ceb44
Zeddicus414 <31261142+Zeddicus414@users.noreply.github.com>
2022-05-16 09:26:05 -0500

    Add sql injection and golden integers.

--- runtime/queries/python/injections.scm
+++ runtime/queries/python/injections.scm
@@ -1,2 +1,6 @@
 ((comment) @injection.content
  (#set! injection.language "comment"))
+
+((string) @injection.content 
+  (#match? @injection.content "^.{3,5}--sql")
+ (#set! injection.language "sql"))
